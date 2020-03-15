7d7cc3f2cb91c29c578603a5bb82a5aa7067c8a0
Rui Fernandes <rui.correia.23@hotmail.com>
2020-03-15 17:14:09 +0000

    Main.css

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,11 +4,15 @@ import AuthPage from "./Auth/AuthPage";
 export default class LoginPage extends Component {
   render() {
     return (
-      <div className="row">
-        <div className="col-12">
-          <h1>Login Covid!</h1>
-          <AuthPage />
-          <a href="/terms">terms and conditions</a>
+      <div className="vc_row row-container">
+        <div className="container-fluid">
+          <div className="row no-gutters row-parent">
+            <div className="vc_column column_parent style-light col-lg-12">
+              <h1 className="h1 mb-adapt-3">Login Covid!</h1>
+              <AuthPage />
+              <a href="/terms" className="button mt-adapt-5">terms and conditions</a>
+            </div>
+          </div>
         </div>
       </div>
     );
