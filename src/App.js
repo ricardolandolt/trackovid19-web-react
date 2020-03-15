b08df3f5d06091edcd49df09e3d9405c97c0a06c
Ricardo Landolt <ricardolandolt@gmail.com>
2020-03-15 15:29:04 +0000

    Router & PrivateRoute; First Pages;

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import HomePage from './components/HomePage';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { PrivateRoute } from "./helpers/PrivateRoute";
+import DashPage from "./components/DashPage";
+import AboutPage from "./components/AboutPage";
+import TermsPage from "./components/TermsPage";
+import LoginPage from "./components/LoginPage";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Switch>
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/terms" component={TermsPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <PrivateRoute exact path="/" component={DashPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
